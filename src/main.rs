e19eb6edaf1b9a01ea3a06040eeb91b8ce6ec106
mrtdurdenthe2 <kai.wil.business@gmail.com>
2025-06-05 04:14:31 +0100

    added deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind };
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+    DefaultTerminal, Frame,
+
+};
 fn main() {
     println!("Hello, world!");
 }
