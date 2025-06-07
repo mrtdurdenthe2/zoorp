5e47e622735c4bf5863f7a66a7bc8de232ac5499
mrtdurdenthe2 <kai.wil.business@gmail.com>
2025-06-07 22:19:21 +0100

    dev branch, made a better structure for rendering

--- src/main.rs
+++ src/main.rs
@@ -11,30 +11,60 @@ use ratatui::{
     DefaultTerminal, Frame,
 
 };
-fn main() {
-    let terminal = ratatui::init();
-    let result = run(terminal);
 
+#[derive(Default)]
+struct App {
+    should_quit: bool
+}
+struct MyHeaderWidget {
+
+}
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = App::default().run(&mut terminal);
     ratatui::restore();
 
+    app_result
 }
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.should_quit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
 
-fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
-    loop {
-        terminal.draw(render)?;
-
-        if let Event::Key(key) = event::read()? {
-            match key.code {
-                event::KeyCode::Esc => {
-                    break;
-                },
-                _ => {}
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            // it's important to check that the event is a key press event as
+            // crossterm also emits key release and repeat events on Windows.
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
             }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Esc => self.exit(),
+            _ => {}
         }
     }
-    Ok(())
-}
 
-fn render(frame: &mut Frame) {
-    Paragraph::new("Hello").render(frame.area(), frame.buffer_mut());
+    fn exit(&mut self) {
+        self.should_quit = true
+    }
+
+}
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        // ...
+        Paragraph::new("Header text").render(Rect::new(0, 0, area.width, 1), buf);
+    }
 }
