715514272114609b2a249b8aa046a483b9bddc89
mrtdurdenthe2 <kai.wil.business@gmail.com>
2025-06-20 21:34:38 +0100

    rust is so shit

--- src/collecter.rs
+++ src/collecter.rs
@@ -1,2 +1,31 @@
 // this is where the data will be requested
 
+use reqwest::header::{ValuesMut, USER_AGENT, WARNING};
+use reqwest::Error;
+use serde::{Deserialize, Serialize};
+use std::env;
+
+pub enum time_series {
+    Daily(String),
+    Hourly(String),
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct OHLCpacket {
+    // called it a bag because its bigger than a packet. genius i know
+    open: f64,
+    high: f64,
+    low: f64,
+    close: f64,
+    volume: u64,
+}
+
+pub fn daily_data(ts: &str, symbol: &str) -> Result<(), Error> {
+    let key: String = env::var("ZOORPKEY").expect("No API key set");
+    let vantage_url =
+        format!("https://www.alphavantage.co/query?function={ts}&symbol={symbol}&apikey={key}");
+    let response = reqwest::blocking::get(vantage_url)?.text()?;
+
+    response;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,81 @@
-use std::io;
+use std::{collections, default, io};
 
-use crossterm::{event::{self, Event, KeyCode, KeyEvent, KeyEventKind }, terminal};
+use collecter::daily_data;
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    terminal,
+};
 use ratatui::{
     buffer::Buffer,
     layout::{self, Layout, Rect},
+    prelude::*,
     style::Stylize,
     symbols::border,
     text::{Line, Text},
-    widgets::{Block, Paragraph, Widget, Borders},
+    widgets::{Block, Borders, Paragraph, Widget},
     DefaultTerminal, Frame,
-    prelude::*,
 };
+use reqwest::Result;
 
+mod collecter;
 
 #[derive(Default)]
-struct App { // Keep the "data" for the app in here 
-    should_quit: bool
+enum timeframes {
+    Hourly,
+    #[default]
+    Daily,
+    Weekly,
+    Monthy,
 }
-struct MyHeaderWidget {
 
+#[derive(Default)]
+struct timeframe {
+    kind: timeframes,
+    value: String,
 }
 
-fn ui (frame: &mut Frame, _app: &App) { // used in impl App::Run, which is used by main
+#[derive(Default)]
+struct App {
+    // Keep the core functional data for the app in here
+    should_quit: bool,
+    current_tf: timeframe,
+    current_sbol: String,
+    data: String,
+}
+
+fn ui(frame: &mut Frame, _app: &App) {
+    // used in impl App::Run, which is used by main
     let chunks = Layout::default()
-    .direction(Direction::Horizontal)
-    .constraints(&vec![ 
-        Constraint::Percentage(50),
-        Constraint::Percentage(50)
-    ])
-    .split(frame.area());
+        .direction(Direction::Horizontal)
+        .constraints(&vec![
+            Constraint::Percentage(50),
+            Constraint::Percentage(50),
+        ])
+        .split(frame.area());
 
-
-    let left_pane = Block::default().title_top("LEFT PANE").borders(Borders::ALL);
-    let right_pane = Block::default().title_top("RIGHT PANE").borders(Borders::ALL);
+    let left_pane = Block::default()
+        .title_top("LEFT PANE")
+        .borders(Borders::ALL);
+    let right_pane = Block::default()
+        .title_top("RIGHT PANE")
+        .borders(Borders::ALL);
     frame.render_widget(left_pane, chunks[0]);
     frame.render_widget(right_pane, chunks[1]);
-    }
+
+    let mut data_return = Text::raw(String::from("{}"));
+}
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
     let app_result = App::default().run(&mut terminal);
     ratatui::restore();
-
     app_result
 }
 
 impl App {
+    fn set_chart_data(&mut self) -> Result<(), reqwest::Error> {
+        let newdata = collecter::daily_data("TIME_SERIES_DAILY", "TSLA")?;
+        Ok(())
+    }
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.should_quit {
             terminal.draw(|frame| self.draw(frame))?;
@@ -63,25 +94,31 @@ impl App {
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
                 self.handle_key_event(key_event)
             }
-            _ => {}
+            _ => Ok(()),
         };
         Ok(())
     }
 
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
+    fn handle_key_event(&mut self, key_event: KeyEvent) -> Result<(), reqwest::Error> {
         match key_event.code {
-            KeyCode::Esc => self.exit(),
-            _ => {}
+            KeyCode::Esc => {
+                self.exit();
+                Ok(())
+            }
+
+            KeyCode::Char(d) => {
+                self.set_chart_data();
+                Ok(())
+            }
+            _ => Ok(()),
         }
     }
 
     fn exit(&mut self) {
         self.should_quit = true
     }
-
 }
 impl Widget for &App {
-    
     fn render(self, area: Rect, buf: &mut Buffer) {
         // ...
         Paragraph::new("Header text").render(Rect::new(0, 0, area.width, 1), buf);
